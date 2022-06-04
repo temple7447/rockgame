2bbeb80bc054e6fb66260bbafbedbfff4a53c38d
temple7447 <Starukido@gmail.com>
2022-06-04 08:29:07 +0100

    first commit

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import random
 
-draw = "it’s a tie and the winner is computer"
+def game():
+      
+    if player == computeraction:
+        print(draw)
+        
+    elif (player == "R" and computeraction == "S"):
+        print('Rock beats Scissors and the winner is player')
+        
+    elif (player == "P" and computeraction == "R"):
+        print('Paper beats Rock and the winner is player')
+        
+    elif (player == "S" and computeraction == "P"):
+        print('Scissors beats Paper and the winner is player')
+        
+    elif (player == "S" and computeraction == "R"):
+        print('Rock beats Scissors and the winner is computer')
+        
+    elif (player == "R" and computeraction == "P"):
+        print('Paper beats Rock and the winner is computer')
+        
+    elif (player == "P" and computeraction == "S"):
+        print('Scissors beats Paper and the winner is computer')
 
 
 
-computer = ["R","P","S"]
-player = input("Enter any of this option R,P,S: ")
+while True:
+    draw = "it’s a tie and the winner is computer"
 
-computeraction = random.choice(computer)
-print(computeraction)
 
-if player == computeraction:
-    print(draw)
+
+    computer = ["R","P","S"]
+    player = input("Enter any of this option R,P,S: ")
+
+    computeraction = random.choice(computer)
+    print(computeraction)
+      
+    game()
     
-elif (player == "R" and computeraction == "S"):
-    print('Rock beats Scissors and the winner is player')
-    
-elif (player == "P" and computeraction == "R"):
-    print('Paper beats Rock and the winner is player')
-    
-elif (player == "S" and computeraction == "P"):
-    print('Scissors beats Paper and the winner is player')
-    
-elif (player == "S" and computeraction == "R"):
-    print('Rock beats Scissors and the winner is computer')
-    
-elif (player == "R" and computeraction == "P"):
-    print('Paper beats Rock and the winner is computer')
-    
-elif (player == "P" and computeraction == "S"):
-    print('Scissors beats Paper and the winner is computer')
+    player_again = input ('play again? (y/n):  ')
+    if player_again.lower() != 'y':
+        break
     
     
